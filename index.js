7138a60232caa75e5e6d5e74567b88e6b33f12ec
Payam Abdullah <payam97abubakr@gmail.com>
2021-04-23 20:00:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,4 +10,11 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(timeString){
+  const array = timeString.split(":");
+const seconds = (parseInt(array[0], 10) * 60 * 60) + (parseInt(array[1], 10) * 60) + parseInt(array[2], 10)
+
+console.log(seconds);
+}
+handleClick();
 /* Write your implementation of displayMessage() */
